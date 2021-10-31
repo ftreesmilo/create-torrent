c279ed3f4bca9f195f7040e3deb915e0142035fb
ftreesmilo <58193105+ftreesmilo@users.noreply.github.com>
2021-10-31 19:36:12 -0400

    add tracker

--- index.js
+++ index.js
@@ -22,7 +22,8 @@ const announceList = [
   ['udp://explodie.org:6969'],
   ['udp://tracker.empire-js.us:1337'],
   ['wss://tracker.btorrent.xyz'],
-  ['wss://tracker.openwebtorrent.com']
+  ['wss://tracker.openwebtorrent.com'],
+  ['wss://tracker.fastcast.nz']
 ]
 
 /**
